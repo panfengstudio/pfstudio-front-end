388284e7fed3a356bc35dfc9233818b30134d5ac
杨丰畅 <ayang818@qq.com>
2019-09-02 21:16:58 +0800

    (Complete)完成导航栏

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+import axios from "axios"
+Vue.prototype.$http = axios.create({
+  baseURL: "http://localhost:1080"
+})
 
 new Vue({
   router,
